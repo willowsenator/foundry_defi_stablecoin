b796f7f24132eabf97672195479a89974c12e309
willowsenator <willowsenator@gmail.com>
2025-02-21 22:53:38 +0100

    Format OracleLib

--- src/libraries/OracleLib.sol
+++ src/libraries/OracleLib.sol
@@ -9,21 +9,25 @@ import {AggregatorV3Interface} from "@chainlink/contracts/v0.8/interfaces/Aggreg
  * @notice This library is used in chainlink oracles
  * If a price is stale, the function will revert and render DSCEngine unusable - this is a feature, not a bug.
  * We want the DSCEngine to freeze if the price is stale.
- * 
+ *
  * So if the Chainlink Oracle is down, the DSCEngine will lock your money until the Oracle is back up.
  */
-library  OracleLib  {
+library OracleLib {
     error OracleLib__StalePrice();
+
     uint256 private constant TIMEOUT = 3 hours; // 3 * 60 * 60 = 10800 seconds
 
-    function staleCheckLatestRoundData(AggregatorV3Interface priceFeed) public view 
-    returns (uint80, int256, uint256, uint256, uint80) {
-        (uint80 roundID, int256 price, uint256 startedAt, uint256 timestamp, uint80 answeredInRound) = priceFeed.latestRoundData();
+    function staleCheckLatestRoundData(AggregatorV3Interface priceFeed)
+        public
+        view
+        returns (uint80, int256, uint256, uint256, uint80)
+    {
+        (uint80 roundID, int256 price, uint256 startedAt, uint256 timestamp, uint80 answeredInRound) =
+            priceFeed.latestRoundData();
         uint256 secondsSince = block.timestamp - timestamp;
         if (secondsSince > TIMEOUT) {
             revert OracleLib__StalePrice();
         }
         return (roundID, price, startedAt, timestamp, answeredInRound);
-        
     }
 }
